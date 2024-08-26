ef4f049aa8032c468d07950409b21697a3635571
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2024-08-26 21:38:50 +0530

    changes

--- src/app/components/utils/SearchBar.jsx
+++ src/app/components/utils/SearchBar.jsx
@@ -3,7 +3,7 @@ import { FiSearch } from "react-icons/fi";
 
 const SearchBar = () => {
   return (
-    <div className="text-primary bg-accent w-[366px] text-[16px] rounded-[30px] flex justify-between items-center px-4 py-1 ">
+    <div className="text-primary bg-accent w-[300px] sm:w-[366px] text-[16px] rounded-[30px] flex justify-between items-center px-4 py-1 ">
         Suchen
         <FiSearch className=' text-2xl font-extrabold'/>
 
